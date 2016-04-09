2c238e15234d606c4dd53c8e7eaa547a6932e5ba
Sri Haritha Mallampati <Sri Haritha Mallampati>
2016-04-09 22:18:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/haritha/vehical/Display.java
@@ -0,0 +1,21 @@
+package com.example.haritha.vehical;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Haritha on 09/04/2016.
+ */
+public class Display extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+        String username = getIntent().getStringExtra("Username");
+
+        TextView tv = (TextView)findViewById(R.id.TVusername);
+        tv.setText(username);
+
+    }
+}
